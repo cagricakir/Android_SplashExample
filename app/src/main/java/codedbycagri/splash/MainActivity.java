cd16ba53f293a8488b977a06a8c3a110cf0ebd85
cagricakir <cagricakir995@gmail.com>
2018-03-04 14:09:49 +0300

    Progress bar is added to the project.

--- app/src/main/java/codedbycagri/splash/MainActivity.java
+++ app/src/main/java/codedbycagri/splash/MainActivity.java
@@ -3,6 +3,7 @@ package codedbycagri.splash;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +12,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Thread MyScreen = new Thread(){
+        final ProgressBar loading = (ProgressBar)findViewById(R.id.progressBar);
+        loading.setMax(5000);
+
+        final Thread MyScreen = new Thread(){
             public void run(){
                 try{
-                    sleep(5000);
+                    sleep(1000);
+                    loading.setProgress(1000);
+                    sleep(1000);
+                    loading.setProgress(2000);
+                    sleep(1000);
+                    loading.setProgress(3000);
+                    sleep(1000);
+                    loading.setProgress(4000);
+                    sleep(1000);
+                    loading.setProgress(5000);
+
+
                     startActivity(new Intent(getApplicationContext(), SplashClass.class));
                 }
                 catch (InterruptedException e){
