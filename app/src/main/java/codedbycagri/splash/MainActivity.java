8d4303b7da94d544d2bf7bde6e497eca345eed59
cagricakir <cagricakir995@gmail.com>
2018-03-04 14:57:52 +0300

    First screen is fullscreen, but second screen has System Bar.

--- app/src/main/java/codedbycagri/splash/MainActivity.java
+++ app/src/main/java/codedbycagri/splash/MainActivity.java
@@ -1,16 +1,22 @@
 package codedbycagri.splash;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
 
         final ProgressBar loading = (ProgressBar)findViewById(R.id.progressBar);
         loading.setMax(5000);
--- app/src/main/java/codedbycagri/splash/SplashClass.java
+++ app/src/main/java/codedbycagri/splash/SplashClass.java
@@ -1,14 +1,20 @@
 package codedbycagri.splash;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by vsski on 4.03.2018.
  */
 
-public class SplashClass extends MainActivity{
+public class SplashClass extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
 
 }}
